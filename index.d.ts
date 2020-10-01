6367300f9bf95ffd9f025f94d05c243dd49f64ab
Jayson Harshbarger <jayson.harshbarger@swimlane.com>
2020-10-01 13:21:51 -0600

    build and readme

--- index.d.ts
+++
@@ -1,2 +0,0 @@
-export * from './lib/regexps';
-export * from './lib/matchers';
---
+++ src/lib/assertion.ts
@@ -0,0 +1,22 @@
+import { DiffOptions } from '@open-wc/semantic-dom-diff/get-diffable-html';
+
+import { getDom, clean, disambiguateArgs } from './util';
+
+export const chaiDomMatch = (chai: Chai.ChaiStatic, utils: Chai.ChaiUtils) => {
+  function assertDomMatch(this: Chai.AssertionStatic, re: RegExp, ...args: [string | DiffOptions, DiffOptions]) {
+    const [message, options] = disambiguateArgs(args) as [string, DiffOptions];
+
+    const negate = utils.flag(this, 'negate');
+    const object = utils.flag(this, 'object');
+
+    const dom = clean(getDom(object), options);
+
+    if (negate) {
+      new chai.Assertion(dom).not.match(re, message);
+    } else {
+      new chai.Assertion(dom).match(re, message);
+    }
+  };
+
+  chai.Assertion.addMethod('domMatch', assertDomMatch);
+};
